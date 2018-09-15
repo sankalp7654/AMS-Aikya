47f263f79bbca41614ec6eb4484290bac896760b
sankalp7654 <sankalp.saxena.sta@gmail.com>
2018-09-16 00:10:59 +0530

    added feature to add subject for teacher portal

--- AttendanceManagement/Controllers/TeacherTeachesStudentController.cs
+++ AttendanceManagement/Controllers/TeacherTeachesStudentController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AttendanceManagement.Models;
+using AttendanceManagement.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,8 +10,58 @@ namespace AttendanceManagement.Controllers
 {
     public class TeacherTeachesStudentController : Controller
     {
+        private AttendanceManagementDBEntities1 db = new AttendanceManagementDBEntities1();
+        private static TeacherTeachesStudentModel ttsmodel = new TeacherTeachesStudentModel();
+
         // GET: TeacherTeachesStudent
         public ActionResult Index()
+        {
+            ViewBag.Department_DID = new SelectList(db.Departments, "DID", "Name");
+            ViewBag.Section = new SelectList(db.Students, "Section", "Section");
+            ViewBag.Sem = new SelectList(db.Students, "Sem", "Sem");
+            ViewBag.Sub_Code = new SelectList(db.Subjects, "SubCode", "SubCode");
+
+            return View();
+        }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(TeacherTeachesStudentModel classViewModel)
+        {
+            ttsmodel.Sub_Code = classViewModel.Sub_Code;
+            ttsmodel.Department_DID = classViewModel.Department_DID;
+            ttsmodel.Sem = classViewModel.Sem;
+            ttsmodel.Section = classViewModel.Section;
+
+
+            AttendanceManagementDBEntities1 db = new AttendanceManagementDBEntities1();
+
+
+            var customer = db.AspNetUsers.FirstOrDefault(u => u.Email == User.Identity.Name);
+            var teacher = db.Teachers.FirstOrDefault(u => u.REFID == customer.Id);
+
+
+
+            classViewModel.Students = db.Students.Where(u => u.Section == classViewModel.Section).Where(u => u.Sem == classViewModel.Sem).Where(u => u.Department_DID == classViewModel.Department_DID).Where(u => u.Subject_SubCode == classViewModel.Sub_Code).ToList();
+            for (int i = 0; i < classViewModel.Students.Count; i++)
+            {
+                Teacher_Teaches_Student teacherTeachesStudent = new Teacher_Teaches_Student();
+                teacherTeachesStudent.Teacher_TID = teacher.TID;
+                teacherTeachesStudent.Subject_SubCode = classViewModel.Sub_Code;
+                teacherTeachesStudent.Student_USN = classViewModel.Students[i].USN;
+                db.Teacher_Teaches_Student.Add(teacherTeachesStudent);
+                db.SaveChanges();
+
+            }
+
+            return RedirectToAction("Index", "TeachersPortal");
+        
+        }
+
+
+        public ActionResult Invalid()
         {
             return View();
         }
---
+++ AttendanceManagement/Models/TeacherTeachesStudentModel.cs
@@ -0,0 +1,19 @@
+﻿using AttendanceManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AttendanceManagement.Models
+{
+    public class TeacherTeachesStudentModel
+    {
+   
+        public string Department_DID { get; set; }
+        public int Sem { get; set; }
+        public string Section { get; set; }
+        public string Sub_Code { get; set; }
+        public List<Student> Students { get; set; }
+    }
+}
